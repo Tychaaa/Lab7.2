e8e2d3aaac99fee954eb6ed4abf98ba484f0eb19
Timofey Tychkov <timofeytychkov@gmail.com>
2023-10-22 12:18:38 +0700

    Реализовал методы ввода информации о магазине, вывода информации о всем магазине, а также об отдельных его элементах. Начал реализацию главного меню

--- src/lab/Main.java
+++ src/lab/Main.java
@@ -4,43 +4,93 @@ import java.util.Scanner;
 
 public class Main {
 
+    public static boolean StoreCreated = false;
+    public static boolean OrderCreated = false;
     public static void main(String[] args) {
+
+        int input;
+        Store store1 = new Store();
 
         System.out.println("\t~~Первая программа на Java~~");
         System.out.println("Главное меню:");
         System.out.println("1. Создать магазин пластинок");
-        System.out.println("2. Посмотреть информацию о сотрудниках");
-        System.out.println("3. Посмотреть ассортимент магазина");
-        System.out.println("4. Создать заказ");
-        System.out.println("5. Посмотреть информацию о заказе\n");
+        System.out.println("2. Посмотреть информацию о магазине");
+        System.out.println("3. Посмотреть информацию о сотрудниках");
+        System.out.println("4. Посмотреть ассортимент магазина");
+        System.out.println("5. Создать заказ");
+        System.out.println("6. Посмотреть информацию о заказе");
+        System.out.println("0. Выход из программы\n");
 
-        int input;
+        Scanner scan = new Scanner(System.in);
 
         do {
             System.out.print("Выберите действие: ");
-            Scanner scan = new Scanner(System.in);
+            while (!scan.hasNextInt()) {
+                System.out.print("Некорректный ввод!\nВыберите действие: ");
+                scan.next(); // Очищаем буфер после некорректного ввода
+            }
             input = scan.nextInt();
 
             switch (input) {
+                // 1. Создание магазина
                 case 1:
-                    System.out.println("1 действие");
+                    // Создание нового магазина
+                    if(!StoreCreated) {
+                        store1.inputStoreInfo();
+                        StoreCreated = true;
+                    // Пересоздание магазина, если он уже был создан
+                    }else{
+                        System.out.println("Магазин уже создан. Хотите пересоздать магазин? (да/нет)");
+                        String response = scan.nextLine().toLowerCase();
+
+                        if (response.equals("да")) {
+                            store1.inputStoreInfo();
+                        } else {
+                            System.out.println("Магазин не будет пересоздан.");
+                        }
+                    }
                     break;
+                // 2. Информация о магазине
                 case 2:
-                    System.out.println("2 действие");
+                    if(StoreCreated)
+                        store1.outputStoreInfo();
+                    else
+                        System.out.println("Прежде чем воспользоваться этой функцией, создайте магазин!");
                     break;
+                // 3. Информация о сотрудниках
                 case 3:
-                    System.out.println("3 действие");
+                    if(StoreCreated)
+                        store1.outputEmployees();
+                    else
+                        System.out.println("Прежде чем воспользоваться этой функцией, создайте магазин!");
                     break;
+                // 4. Информация о пластинках
                 case 4:
-                    System.out.println("4 действие");
+                    if(StoreCreated)
+                        store1.outputVinylRecords();
+                    else
+                        System.out.println("Прежде чем воспользоваться этой функцией, создайте магазин!");
                     break;
                 case 5:
-                    System.out.println("5 действие");
+                    if(StoreCreated) {
+                        System.out.println();
+                        StoreCreated = true;
+                    }else
+                        System.out.println("Прежде чем воспользоваться этой функцией, создайте магазин!");
+                    break;
+                case 6:
+                    if(StoreCreated && OrderCreated)
+                        System.out.println();
+                    else
+                        System.out.println("Ни одного заказа не найдено!");
+                    break;
+                case 0:
+                    System.out.println("До свидания!");
                     break;
                 default:
                     System.out.println("Некорректный выбор!\n");
             }
-        }while (input < 1 || input > 5);
+        }while (input != 0);
     }
 
 }
--- src/lab/Store.java
+++ src/lab/Store.java
@@ -1,5 +1,7 @@
 package lab;
 
+import java.util.Scanner;
+
 public class Store {
     private static final int MAX_RECORDS = 100; // Максимальное количество виниловых пластинок в магазине
     private static final int MAX_EMPLOYEES = 50; // Максимальное количество сотрудников магазина
@@ -31,21 +33,101 @@ public class Store {
         this.storeAddress = storeAddress;
     }
 
-    public VinylRecord[] getVinylRecordsInStore() {
-        return vinylRecordsInStore;
+    // Метод для ввода информации о магазине
+    public void inputStoreInfo() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("\t~~Создание магазина виниловых пластинок~~");
+
+        System.out.print("Введите название магазина: ");
+        setStoreName(scanner.nextLine());
+
+        System.out.print("Введите адрес магазина: ");
+        setStoreAddress(scanner.nextLine());
+        System.out.println("-------------------------------------------");
+
+        System.out.print("Введите количество виниловых пластинок, которые вы хотите добавить: ");
+        int numVinylRecords = scanner.nextInt();
+        scanner.nextLine(); // Очищаем буфер после ввода числа
+
+        System.out.println("-------------------------------------------");
+        vinylRecordsInStore = new VinylRecord[numVinylRecords];
+        for (int i = 0; i < numVinylRecords; ++i) {
+            System.out.println("Ввод информации о виниловой пластинке #" + (i + 1));
+            vinylRecordsInStore[i] = new VinylRecord();
+            vinylRecordsInStore[i].inputVinylRecord();
+            System.out.println("-------------------------------------------");
+        }
+
+        System.out.print("Введите количество сотрудников, которых вы хотите добавить: ");
+        int numEmployees = scanner.nextInt();
+        scanner.nextLine(); // Очищаем буфер после ввода числа
+
+        System.out.println("-------------------------------------------");
+        employeesInStore = new Employee[numEmployees];
+        for (int i = 0; i < numEmployees; ++i) {
+            System.out.println("Ввод информации о сотруднике #" + (i + 1));
+            employeesInStore[i] = new Employee();
+            employeesInStore[i].inputEmployee();
+            System.out.println("-------------------------------------------");
+        }
     }
-
-    public void setVinylRecordsInStore(VinylRecord[] vinylRecordsInStore) {
-        this.vinylRecordsInStore = vinylRecordsInStore;
+    // Метод для вывода информации о введенных сотрудниках
+    public void outputEmployees() {
+        System.out.println("Информация о сотрудниках магазина \"" + getStoreName() + "\"");
+        int employeeNumber = 1; // Счетчик для номера сотрудника
+        System.out.println("---------------------------");
+        for (Employee employee : employeesInStore) {
+            if (employee != null) {
+                System.out.println("Сотрудник #" + employeeNumber);
+                System.out.println("Имя: " + employee.getFirstName());
+                System.out.println("Фамилия: " + employee.getLastName());
+                System.out.println("Должность: " + employee.getPosition());
+                System.out.println("Зарплата: " + employee.getSalary());
+                System.out.println("---------------------------");
+                employeeNumber++;
+            }
+        }
     }
-
-    public Employee[] getEmployeesInStore() {
-        return employeesInStore;
+    // Метод для вывода информации о введенных виниловых пластинках
+    public void outputVinylRecords() {
+        System.out.println("Информация о виниловых пластинках в магазине \"" + getStoreName() + "\"");
+        int recordNumber = 1; // Счетчик для номера пластинки
+        System.out.println("---------------------------");
+        for (VinylRecord record : vinylRecordsInStore) {
+            if (record != null) {
+                System.out.println("Виниловая пластинка #" + recordNumber);
+                System.out.println("Название альбома: " + record.getAlbumName());
+                System.out.println("Исполнитель: " + record.getArtist());
+                System.out.println("Год выпуска: " + record.getYear());
+                System.out.println("Жанр: " + record.getGenre());
+                System.out.println("Цена: " + record.getPrice());
+                System.out.println("Количество в наличии: " + record.getQuantity());
+                System.out.println("---------------------------");
+                recordNumber++;
+            }
+        }
     }
-
-    public void setEmployeesInStore(Employee[] employeesInStore) {
-        this.employeesInStore = employeesInStore;
+    // Метод для вывода всей информации о магазине
+    public void outputStoreInfo() {
+        System.out.println("Информация о магазине:");
+        System.out.println("Название магазина: " + getStoreName());
+        System.out.println("Адрес магазина: " + getStoreAddress());
+        System.out.println("Пластинки в наличии:");
+        int recordNumber = 1;
+        for (VinylRecord record : vinylRecordsInStore) {
+            if (record != null) {
+                System.out.println(recordNumber + ") " +record.getAlbumName() + " - " + record.getArtist() + " (" + record.getQuantity() + " шт.)");
+                recordNumber++;
+            }
+        }
+        System.out.println("Сотрудники магазина:");
+        int employeeNumber = 1; // Счетчик для номера сотрудника
+        for (Employee employee : employeesInStore) {
+            if (employee != null) {
+                System.out.println(employeeNumber + ") " + employee.getFirstName() + " " + employee.getLastName() + " - " + employee.getPosition());
+                employeeNumber++;
+            }
+        }
     }
-
-
 }
